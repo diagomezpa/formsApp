8e954ca39db32b1414e54e63b6f34775381083ac
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-15 21:49:20 -0500

    class 17 veideo 237. Primeros pasos en formularios reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class BasicPageComponent {
+  // esta es una forma de crear formulario
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''), // 1 valor por defecto 2 validaciones sincronos, 3 validaciones asincronas
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
 
+  // creando el mismo form con formbuilder puede escoger la primera o la segunda
+
+  public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  });
+  constructor(private fb: FormBuilder) {}
+
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 }
