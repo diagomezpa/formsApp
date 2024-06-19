c97c69d4594cfcfefd8f9f98dd7cf847bb658afc
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-19 16:12:08 -0500

    clase 17 video 138 Forms Vaidator - Validador incluido en Angular

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validator,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -8,21 +14,22 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 export class BasicPageComponent {
   // esta es una forma de crear formulario
   // public myForm: FormGroup = new FormGroup({
-  //   name: new FormControl(''), // 1 valor por defecto 2 validaciones sincronos, 3 validaciones asincronas
-  //   price: new FormControl(0),
+  //   name: new FormControl('',[]), // 1 valor por defecto 2 validaciones sincronos, 3 validaciones asincronas
+  //   price: new FormControl(0,),
   //   inStorage: new FormControl(0),
   // });
 
   // creando el mismo form con formbuilder puede escoger la primera o la segunda
 
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
     inStorage: [0],
   });
   constructor(private fb: FormBuilder) {}
 
   onSave(): void {
+    if (this.myForm.invalid) return;
     console.log(this.myForm.value);
   }
 }
