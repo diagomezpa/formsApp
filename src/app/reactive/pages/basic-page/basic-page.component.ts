96df4e46af38139f2fd2101d225c81d7a934ee45
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-19 16:49:14 -0500

    clase 17 video restablecer y establecer el valor al formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,35 +1,67 @@
-import { Component } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validator,
-  Validators,
-} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6,
+};
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: ``,
+  styles: [],
 })
-export class BasicPageComponent {
-  // esta es una forma de crear formulario
+export class BasicPageComponent implements OnInit {
   // public myForm: FormGroup = new FormGroup({
-  //   name: new FormControl('',[]), // 1 valor por defecto 2 validaciones sincronos, 3 validaciones asincronas
-  //   price: new FormControl(0,),
+  //   name: new FormControl('', []),
+  //   price: new FormControl(0),
   //   inStorage: new FormControl(0),
   // });
-
-  // creando el mismo form con formbuilder puede escoger la primera o la segunda
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(0)]],
-    inStorage: [0],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
+
   constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    // this.myForm.reset( rtx5090 );
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracters.`;
+      }
+    }
+
+    return null;
+  }
+
   onSave(): void {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
     console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 }
