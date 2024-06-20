d0c55704dcec864e8cfc987015f6956f87efb13c
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-19 19:13:17 -0500

    clase 17 video 243. Validaciones Dinámicas

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -15,7 +15,39 @@ export class DynamicPageComponent {
       ['Death Stranding', Validators.required],
     ]),
   });
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
 
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracters.`;
+      }
+    }
+
+    return null;
+  }
   onSumit(): void {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
@@ -24,9 +56,5 @@ export class DynamicPageComponent {
 
     console.log(this.myForm.value);
     this.myForm.reset();
-  }
-
-  get favoriteGames() {
-    return this.myForm.get('favoriteGames') as FormArray;
   }
 }
