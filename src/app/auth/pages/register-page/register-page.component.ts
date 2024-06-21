6bbee9f2b3bd7dac46e8ab0e07653de0ffb866ff
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-21 13:01:51 -0500

    clase 18 video  Formulario de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterPageComponent {
+  constructor(private fb: FormBuilder) {}
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    usernamme: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    password2: ['', [Validators.required]],
+  });
+
+  isValidField() {
+    // Todo: obtener la validación desde un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
