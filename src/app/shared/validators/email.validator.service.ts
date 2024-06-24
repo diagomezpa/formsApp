1991c78a196a0320b9b96ee20b314de9c41d011d
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-24 11:59:27 -0500

    clase 18 video 257 Elaborar el vaidador de correo

--- src/app/shared/validators/email.validator.service.ts
+++ src/app/shared/validators/email.validator.service.ts
@@ -8,34 +8,31 @@ import { delay, Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class EmailValidator implements AsyncValidator {
-  //   validate(control: AbstractControl): Observable<ValidationErrors | null> {
-  //     const email = control.value;
-
-  //     const httpCallObservable = new Observable<ValidationErrors | null>(
-  //       (subscriber) => {
-  //         console.log({ email });
-
-  //         if (email === 'fernando@google.com') {
-  //           subscriber.next({ emailTaken: true });
-  //           subscriber.complete();
-  //           // return;
-  //         }
-
-  //         subscriber.next(null);
-  //         subscriber.complete();
-  //       }
-  //     ).pipe(delay(3000));
-
-  //     return httpCallObservable;
-  //   }
-
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
-    console.log({ email });
-    return of({
-      emailTaken: true,
-    }).pipe(delay(2000));
+    const httpCallObservable = new Observable<ValidationErrors | null>(
+      (subscriber) => {
+        console.log({ email });
+        if (email === 'fernando@google.com') {
+          subscriber.next({ emailTaken: true }); // aca emite el valor
+          subscriber.complete(); // completa y cierra el obserbable
+          return;
+        }
+
+        subscriber.next(null);
+        subscriber.complete();
+      }
+    ).pipe(delay(3000));
+    return httpCallObservable;
   }
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log({ email });
+  //   return of({
+  //     emailTaken: true,
+  //   }).pipe(delay(2000));
+  // }
 }
 
 // return this.http.get<any[]>(`http://localhost:3000/users?q=${ email }`)
